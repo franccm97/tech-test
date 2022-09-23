61bedd2874fc13ab5453dd95e934115573df93da
franccm97 <franccm97@gmail.com>
2022-09-23 20:18:02 +0200

    improving

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import Chapters from "./Chapters.json"
+import { ChapterContainer } from "./Components/containers"
 
 function App() {
+
+
+  const [chapters, setChapters] = useState([])
+  const [selectedChapter, setSelectedChapter] = useState(null)
+  const [selectedSubChapters, setSelectedSubChapters] = useState(null)
+
+
+
+  useEffect(() => {
+    Chapters.map((chapters) => {
+      setChapters(chapters)
+    })
+    // chapters?.subchapters.map((subchapter) => {
+    //   setSelectedSubChapters(subchapter)
+    // })
+
+  }, [selectedChapter, selectedSubChapters])
+
+
+  function handleChapter(e) {
+    setSelectedChapter(e)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        chapters && chapters.map((chapter, index) => {
+          console.log(chapter.subchapters,"im this one");
+          return <>
+            <div onClick={() => handleChapter(chapter.chapterInfo.name)}>
+              {
+                <>
+                  {chapter.chapterInfo.name === selectedChapter ?
+                    <div>
+                      <h3>
+                        {chapter.chapterInfo.name}
+                      </h3>
+                      {/* {
+                        chapter.subchapters.map((subchapter) => {
+                          return (
+
+                            console.log(subchapter)
+                          )
+                        })
+                      } */}
+                    </div>
+                    : <h3>
+                      {chapter.chapterInfo.name}
+                    </h3>}
+                </>
+              }
+
+            </div>
+          </>
+        })
+      }
+    </>
   );
 }
 
---
+++ src/Components/containers.js
@@ -0,0 +1,78 @@
+import styled from "styled-components"
+
+
+export const ChapterContainer = styled.div`
+  width: 98%;
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 6px;
+  height: 44px;
+  padding-top: 0px;
+  margin-bottom: 12px;
+  margin-top: 0px;
+  background-color: white;
+  box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.08);
+`
+
+
+export const SubChapterContainer = styled.div`
+   {
+    width: 98.8% !important;
+    box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 6px;
+    height: 44px;
+    padding-top: 0px;
+    margin-bottom: 9px;
+    margin-top: 8px;
+    background-color: #F7DDC8;
+    border-radius: 6px;
+    min-width: 920px;
+    box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.08);
+  }
+`
+
+
+export const ShowChildDiv = styled.div`
+   {
+    margin-left: 0px;
+    width: 98.8%;
+    background-color:rgba(252, 168, 100, 0.21);
+    padding-top: 10px;
+    box-shadow: rgb(0 0 0 / 8%) 0px 6px 6px;
+    display: block;
+    margin-bottom: 8px;
+    padding-bottom: 8px;
+    margin-top:-8px;
+    padding-left: 8px;
+  }
+`
+
+export const ShowParentDiv = styled.div`
+   {
+    width: 100%;
+    background-color: white;
+    box-shadow: rgb(0 0 0 / 8%) 0px 6px 6px;
+    padding-top: 20px;
+    padding-bottom: 12px;
+    padding-left: 24px;
+    display: block;
+    margin-left: 0px;
+    margin-bottom: 8px;
+    margin-top: -19.5px;
+    min-width: 1020px;
+  }
+`
+
+
+  export const EmptyButton = styled.div`
+  background-color: white;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 135px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: relative;
+`
+
